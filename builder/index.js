e2fb88d2efbe70e127560382938b4609409a4ef8
Omar300HP <omar.essam.se@gmail.com>
2022-01-06 05:01:25 +0200

    add colors to logs

--- builder/index.js
+++ builder/index.js
@@ -5,6 +5,22 @@ class CodeBuilder {
   get spaceIndent() {
     return " ";
   }
+
+  get logger() {
+    return {
+      reset: "\x1b[0m",
+      green: "\x1b[32m",
+      cyan: "\x1b[36m",
+      magneta: "\x1b[35m",
+      colorText(text, color) {
+        return `${color}${text}${this.reset}`;
+      },
+      char(char) {
+        return `${this.magneta}${char}${this.reset}`;
+      },
+    };
+  }
+
   constructor(className) {
     this.className = className;
     this.fields = [];
@@ -17,16 +33,22 @@ class CodeBuilder {
   }
 
   toStingImpl() {
-    let lines = `class ${this.className} {\n`;
+    let lines = `${this.logger.colorText("class", this.logger.cyan)}`;
+    lines += ` ${this.logger.colorText(this.className, this.logger.green)} `;
+    lines += `${this.logger.char("{")}\n`;
     if (this.fields.length) {
-      const fieldsParams = `(${this.fields.join(", ")})`;
-      lines += `${this.tabIndent}constructor${fieldsParams} {\n`;
+      let fieldsParams = `${this.logger.char("(")}`;
+      fieldsParams += `${this.fields.join(", ")}`;
+      fieldsParams += `${this.logger.char(")")}`;
+      lines += `${this.tabIndent}`;
+      lines += `${this.logger.colorText("constructor", this.logger.cyan)} `;
+      lines += `${fieldsParams} ${this.logger.char("{")}\n`;
       for (const field of this.fields) {
         lines += `${this.tabIndent}${this.spaceIndent} this.${field} = ${field};\n`;
       }
-      lines += `${this.tabIndent}}`;
+      lines += `${this.tabIndent}${this.logger.char("}")}`;
     }
-    lines += `${this.fields.length ? "\n" : ""}}`;
+    lines += `${this.fields.length ? "\n" : ""}${this.logger.char("}")}`;
     return lines;
   }
 
@@ -40,9 +62,4 @@ let cb = new CodeBuilder("Person")
   .addField("age")
   .addField("job");
 
-eval(
-  cb.toString() +
-    ";" +
-    "const person = new Person('omar', 28, 'front-end plumber');" +
-    "console.log(person);"
-);
+console.log(cb.toString());
